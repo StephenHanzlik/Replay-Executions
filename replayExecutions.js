d058ac7a6170863fa408f6ef63612f8fad56ef07
Admin User <adminuser@elements-mac114.local>
2018-07-12 17:49:43 -0600

    added basic error checking still need to vet it better

--- replayExecutions.js
+++ replayExecutions.js
@@ -10,17 +10,16 @@ const prompt = require('prompt');
 const apiUrl = 'https://api.cloud-elements.com/elements/api-v2'
 //const startTime = 1529004600000;
 //const endTime = 1529020800000;
-//console.log("");
 console.log("Please provide the following information to replay past executions:");
 
 prompt.start();
-prompt.get(['Environment (staging or production?)','Formula Instance ID', 'User Token', 'Organization Token', 'Start Time (i.e. 2018/01/01 00:00:00)', 'End Time (i.e. 2018/01/01) 00:00:00'], function (err, result) {
-  const authHeader = `User ${result['User Token']}, Organization ${result['Organization Token']}`
-  const formulaInstanceId = `${result['Formula Instance ID']}`;
-  const startTime = result['Start Time (i.e. 2018/01/01 00:00:00)'];
-  const endTime = result['End Time (i.e. 2018/01/01) 00:00:00'];
-  console.log(startTime);
-  console.log(endTime);
+prompt.get(['Environment (staging or production?)','Formula Instance ID', 'User Token', 'Organization Token', 'Start Time UTC(i.e. 2018/01/01 00:00:00)', 'End Time UTC(i.e. 2018/01/01 00:00:00)'], function (err, result) {
+  // const authHeader = `User ${result['User Token']}, Organization ${result['Organization Token']}`
+  // const formulaInstanceId = `${result['Formula Instance ID']}`;
+  const authHeader = `User a/71yZpaoDHME6mCtSL50EVPRAsiunYcteMe1kbo9lo=, Organization UbK7Os/tsG7Q5XCj2hf8hJi6S1fOuy2WZuMR6IEjFyU=`
+  const formulaInstanceId = `238739`;
+  const startTime = Date.parse(result['Start Time (i.e. 2018/01/01 00:00:00)']);
+  const endTime = Date.parse(result['End Time (i.e. 2018/01/01 00:00:00)']);
 
   console.log(`Replaying executions from: ${new Date(startTime)} to ${new Date(endTime)}`);
   console.log(`Using Authorization Header: ${authHeader}`);
@@ -58,16 +57,23 @@ prompt.get(['Environment (staging or production?)','Formula Instance ID', 'User 
     });
   }
 
-  getExecutions().then(function (response) {
-      console.log("Successfully retrieved executions");
-      return response.filter((execution) => {
-        const executionTime = new Date(execution.createdDate).getTime();
-        return execution.status === 'failed' && executionTime > startTime && executionTime < endTime;
-      }).forEach((execution) => {
-        replayExecution(execution.id);
-      });
-    })
-    .catch(function (err) {
-      console.log(`Error fetching executions: ${err}`)
-    })
+  if(!isNaN(startTime) && !isNaN(endTime) && startTime < endTime){
+    getExecutions().then(function (response) {
+        console.log("Successfully retrieved executions");
+        return response.filter((execution) => {
+          const executionTime = new Date(execution.createdDate).getTime();
+          return execution.status === 'failed' && executionTime > startTime && executionTime < endTime;
+        }).forEach((execution) => {
+          replayExecution(execution.id);
+        });
+      })
+      .catch(function (err) {
+        console.log(`Error fetching executions: ${err}`)
+      })
+  } else if(isNaN(startTime) || isNaN(endTime)){
+    console.log("Please enter a valid date format");
+  } else if(startTime >= endTime){
+    console.log("")
+  }
+
 });
